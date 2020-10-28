bcfbe0567e9b8c9c97d3a9c32b8cd098ad55a228
Matthew Swan <swanm86@gmail.com>
2020-10-27 21:33:01 -0500

    add day 4 part 2

---
+++ day_4_part_2/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"crypto/md5"
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	fmt.Println(findSmallestIntFor00000PrefixMD5("iwrupvqb"))
+}
+
+func findSmallestIntFor00000PrefixMD5(str string) int {
+	for i := 1; i > 0; i++ {
+		s := strconv.Itoa(i)
+		input := []byte(str + s)
+		mdSum := md5.Sum(input)
+		if fmt.Sprintf("%x", mdSum)[:6] == "000000" {
+			return i
+		}
+	}
+	return 0
+}
