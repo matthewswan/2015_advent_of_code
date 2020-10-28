15d0eb77044a55016d74ec96b65f3505862a8fc6
Matthew Swan <swanm86@gmail.com>
2020-10-28 00:05:23 -0500

    add day-5-part-2

---
+++ day_5_part_2/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
+
+func main() {
+	input, err := ioutil.ReadFile("day-5-part-2-input.txt")
+	if err != nil {
+		panic(err)
+	}
+	inputSlice := strings.Fields(string(input))
+	niceStrCount := countNiceStrings(inputSlice)
+	fmt.Printf("You have %d nice strings!\n", niceStrCount)
+}
+
+func countNiceStrings(input []string) int {
+	niceStrings := make([]string, 0, len(input))
+	for _, str := range input {
+		switch {
+		case duplicatePairMet(str) && duplicateLetterWithGap(str):
+			niceStrings = append(niceStrings, str)
+		}
+	}
+	return len(niceStrings)
+}
+
+func duplicatePairMet(str string) bool {
+	for i := 0; i < len(str)-2; i++ {
+		if strings.Count(str, str[i:i+2]) >= 2 {
+			return true
+		}
+	}
+	return false
+}
+
+func duplicateLetterWithGap(str string) bool {
+	for i := 0; i < len(str)-2; i++ {
+		if str[i] == str[i+2] {
+			return true
+		}
+	}
+	return false
+}
